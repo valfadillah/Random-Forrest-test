bcff53152ff5f60b4924054a8e324c6c73d51c9a
Nauval Fadillah D. <141308177+valfadillah@users.noreply.github.com>
2025-02-26 22:45:59 +0700

    Update Anova_test.py

--- Anova_test.py
+++ Anova_test.py
@@ -7,18 +7,18 @@ from statsmodels.formula.api import ols
 # Load dataset
 df = pd.read_csv("sleep_health_lifestyle_dataset.csv")
 
-# Bersihkan nama kolom agar lebih mudah diakses
+# Cleaning name in dataset
 df.columns = df.columns.str.strip().str.replace(" ", "_").str.lower()
 df.rename(columns={"quality_of_sleep_(scale:_1-10)": "quality_of_sleep"}, inplace=True)
 
-# Lakukan uji ANOVA
+# Anova test
 anova_model = ols('quality_of_sleep ~ C(bmi_category)', data=df).fit()
 anova_table = sm.stats.anova_lm(anova_model, typ=2)
 
 # Ekstrak p-value
 anova_p_value = anova_table["PR(>F)"][0]
 
-# Tampilkan hasil ANOVA
+# Anova preview
 print("\n=== Hasil Uji ANOVA ===")
 print(anova_table)
 print(f"\nP-Value: {anova_p_value:.5f}")
